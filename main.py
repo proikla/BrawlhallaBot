af0dd726177f82d2fe1a95a64b6897ee3f166249
proikla <menshikov1s12006@mail ru>
2022-06-11 00:07:56 +0500

    main

--- main.py
+++ main.py
@@ -1,43 +1,44 @@
 from brawlhalla import *
 
-# start
-# picture of username
-pic_name = input('Enter the name of the image: ')
-img = f'images/{pic_name}.png'
-sleep(5)
+if __name__ == '__main__':
+    # start
+    # picture of username
+    pic_name = input('Enter the name of the image: ')
+    img = f'images/{pic_name}.png'
+    sleep(5)
 
-# the code will be executed while the active window is 'Brawlhalla'
-while pt.getActiveWindowTitle() == 'Brawlhalla':
-    # if we are not in menu
-    if locate_lobby() == 0:
-        while True:
-            pos = get_character_pos(img)
-            print(pos)
-            # image not found
-            if pos is None:
-                move_character('space', .1, 'jumping')
-                break
-            # image found
-            else:
-                x = pos.x
-                y = pos.y
-                print('X = ', x)
-                print('Y = ', y, '\n')
-                # offstage left
-                if x < 420:
-                    to_stage('d', con=x < 420)
-                    print(type(x))
+    # the code will be executed while the active window is 'Brawlhalla'
+    while pt.getActiveWindowTitle() == 'Brawlhalla':
+        # if we are not in menu
+        if locate_lobby() == 0:
+            while True:
+                pos = get_character_pos(img)
+                print(pos)
+                # image not found
+                if pos is None:
+                    move_character('space', .1, 'jumping')
                     break
-                # offstage right
-                elif x > 1500:
-                    to_stage('a', con=x > 1500)
-                    print(type(x))
-                    break
-                elif x > 420 or x < 1500:
-                    attack()
-                    break
-    # otherwise, the locateGameStart() function will start a new game.
-    else:
-        print('Starting new game')
-print("Brawlhalla isn't active!")
-sleep(2)
+                # image found
+                else:
+                    x = pos.x
+                    y = pos.y
+                    print('X = ', x)
+                    print('Y = ', y, '\n')
+                    # offstage left
+                    if x < 420:
+                        to_stage('d', con=x < 420)
+                        print(type(x))
+                        break
+                    # offstage right
+                    elif x > 1500:
+                        to_stage('a', con=x > 1500)
+                        print(type(x))
+                        break
+                    elif x > 420 or x < 1500:
+                        attack()
+                        break
+        # otherwise, the locateGameStart() function will start a new game.
+        else:
+            print('Starting new game')
+    print("Brawlhalla isn't active!")
+    sleep(2)
