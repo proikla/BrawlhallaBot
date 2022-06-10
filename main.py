dea74fb679d75c92e69e380f6bb28c33b3d0c808
proikla <menshikov1s12006@mail ru>
2022-06-11 01:27:14 +0500

    main

--- main.py
+++ main.py
@@ -1,17 +1,25 @@
 import os
 
 from brawlhalla import *
-from os import *
+
+
+def pic():
+    banned = ['start.png', 'resume.png']
+    pics_list = os.listdir('images')
+    for i in range(len(banned)):
+        pics_list.remove(banned[i])
+    pics = " ".join(pics_list)
+    print(f'List of images: {pics}')
+    pic_name = input('Enter the name of the image: ').strip()
+    image = f'images/{pic_name}.png'
+    return image
+
 
 if __name__ == '__main__':
     # start
 
     # picture of username
-    pics_list = os.listdir('images')
-    pics = " ".join(pics_list)
-    print(f'List of images: {pics}')
-    pic_name = input('Enter the name of the image: ').strip()
-    img = f'images/{pic_name}.png'
+    img = pic()
     sleep(5)
 
     # the code will be executed while the active window is 'Brawlhalla'
