826b2433ac6f1457edc2e5c162715334c594a244
proikla <menshikov1s12006@mail ru>
2022-06-11 00:52:31 +0500

    functions for brawlhalla

--- brawlhalla.py
+++ brawlhalla.py
@@ -1,7 +1,6 @@
 import pyautogui as pt
 from time import sleep
 
-
 # moves cursor to image
 def navigate_to(image, clicks, off_x=0, off_y=0):
     pos = pt.locateCenterOnScreen(image, confidence=.7)
